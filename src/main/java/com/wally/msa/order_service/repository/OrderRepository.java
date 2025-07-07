871e52af8fa9f6537c7e40ebe84827ebc9fe7fac
reallywally <goldenale5500@gmail.com>
2025-07-07 23:30:47 +0900

    git 리포 초기화

--- src/main/java/com/wally/msa/order_service/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wally.msa.order_service.repository;
-
-import com.wally.msa.order_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
